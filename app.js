259a6a605cb32794ac695cc90ee4727bb6f36431
ZXX <zhang-xuxin@hotmail.com>
2018-06-23 17:20:26 -0400

    third test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
+var loginSSRouter = require('./routes/loginSS');
 var shoppingRouter = require('./routes/shopping');
 var drinksRouter = require('./routes/drinks');
 var snacksRouter = require('./routes/snacks');
@@ -15,41 +15,108 @@ var addsnacksRouter = require('./routes/addsnacks');
 var drinksController = require('./controllers/drinksController');
 var snacksController = require('./controllers/snacksController');
 var app = express();
+var bodyParser = require("body-parser")
+// var flash = require('connect-flash')
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require( './models/user' )
+const session = require("express-session")
+const passport = require('passport')
+const configPassport = require('./config/passport')
+configPassport(passport)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/shopping', shoppingRouter);
+app.use('/loginSS', loginSSRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'zzbbyanana' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/shopping', shoppingRouter);
-app.use('/login', loginRouter);
+// here is where we check on their logged in status
+app.use((req,res,next) => {
+  res.locals.loggedIn = false
+  if (req.isAuthenticated()){
+    console.log("user has been Authenticated")
+    res.locals.user = req.user
+    res.locals.loggedIn = true
+    if (req.user){
+      if (req.user.googleemail=='zxx@brandeis.edu'){
+        console.log("Owner has logged in")
+        res.locals.status = 'admin'
+      } else {
+        console.log('customer has logged in')
+        res.locals.status = 'customer'
+      }
+    }
+  }
+  next()
+})
+
+app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+app.get('/login/authorized',
+        passport.authenticate('google', {
+                successRedirect : '/',
+                failureRedirect : '/loginerror'
+        }));
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  console.log("checking to see if they are authenticated!")
+  // if user is authenticated in the session, carry on
+  res.locals.loggedIn = false
+  if (req.isAuthenticated()){
+    console.log("user has been Authenticated")
+    return next();
+  } else {
+    console.log("user has not been authenticated...")
+    res.redirect('/login');
+  }
+}
+
+app.get('/loginerror', function(req,res){
+  res.render('loginerror',{})
+})
+
+app.get('/login', function(req,res){
+  res.render('login',{})
+})
+// route for logging out
+app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
 
 
 //app.use('/drinks', drinksRouter);
-app.get('/adddrinks', function(req,res){
+app.get('/adddrinks', isLoggedIn, function(req,res){
   console.log("adding the drinks")
   res.render('adddrinks',{})
 });
-app.post('/adddrinks',drinksController.saveDrinks)
-app.use('/addsnacks', addsnacksRouter);
-app.get('/drinks', drinksController.getAllDrinks );
+app.post('/adddrinks', isLoggedIn, drinksController.saveDrinks)
+app.use('/addsnacks', isLoggedIn, addsnacksRouter);
+app.get('/drinks', isLoggedIn, drinksController.getAllDrinks );
 
 //app.use('/snacks', snacksRouter);
-app.get('/addsnacks', function(req,res){
+app.get('/addsnacks', isLoggedIn, function(req,res){
   console.log("adding the snacks")
   res.render('addsnacks',{})
 });
-app.post('/addsnacks', snacksController.saveSnacks)
-app.use('/addsnacks', addsnacksRouter);
-app.get('/snacks', snacksController.getAllSnacks );
+app.post('/addsnacks', isLoggedIn, snacksController.saveSnacks)
+app.use('/addsnacks', isLoggedIn, addsnacksRouter);
+app.get('/snacks', isLoggedIn, snacksController.getAllSnacks );
 
 
 const
@@ -82,3 +149,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- routes/loginSS.js
+++ routes/loginSS.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Shopat' });
+  res.render('loginSS', { title: 'Shopat' });
 });
 
 module.exports = router;
