118e78887902fad4f567025b18e17812920935fb
ZXX <zhang-xuxin@hotmail.com>
2018-06-27 14:34:47 -0400

    switch auth

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '157494346275-f7jgbi6eev27bhpnnlt9pv3j1oskt5s1.apps.googleusercontent.com',
         'clientSecret'  : 'ibZZUgqxw-db2wUTeXB-y5uu',
-        'callbackURL'   : 'http://localhost:5900/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:5900/login/authorized'
     }
 
 };
